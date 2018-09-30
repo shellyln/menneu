23145abe5928a776ba49dbdbda8ea32bbf6341b2
shellyln <shellyln@users.noreply.github.com>
2018-09-30 10:15:28 +0900

    Edit examples.

--- src.dist/app.js
+++ src.dist/app.js
@@ -5,42 +5,42 @@ const escapeHtml = (s) => s
 .replace(/"/g, "&quot;")
 .replace(/'/g, "&#39;");
 
-const start = (async () => {
-const buf = await menneu.render(document.getElementById('md').innerHTML, [1, 2, 3, 4, 5, 6], {
-    inputFormat: 'md',
-    dataFormat: 'object',
-    outputFormat: 'html',
+const start = (async (cf) => {
+    const buf = await menneu.render(Array.from(document.querySelectorAll('script[type="text/markdown"]'), x => x.innerHTML).join(' '), [1, 2, 3, 4, 5, 6], Object.assign({
+        inputFormat: 'md',
+        dataFormat: 'object',
+        outputFormat: 'html',
 
-    title: 'Markdown example',
-    markdownBodyStyle: 'font-family: "Yu Gothic Medium", YuGothic, meiryo, "Microsoft JhengHei", "Microsoft YaHei", "SimHei", helvetica, arial, sans-serif;',
-    globals: {
-        "$now": () => (new Date).toLocaleDateString('en-US'),
-        "$to-locale-string": (...args) => args.slice(-1)[0].toLocaleString(...(args.slice(0, -1))),
-        "$dir": (...args) => console.dir(...args),
-        "qwerty": "asdfgh",
-    },
-    components: {
-        Greeting: (props) => `Hello, ${props.to}! ${props.children}`,
-    },
-    markdownCustomContainers: [{
-        name: 'content',
-    }, {
-        name: 'spoiler',
-        validate: (params) => {
-            return params.trim().match(/^spoiler\s+(.*)$/);
+        title: 'Markdown example',
+        markdownBodyStyle: 'font-family: "Yu Gothic Medium", YuGothic, meiryo, "Microsoft JhengHei", "Microsoft YaHei", "SimHei", helvetica, arial, sans-serif;',
+        globals: {
+            "$now": () => (new Date).toLocaleDateString('en-US'),
+            "$to-locale-string": (...args) => args.slice(-1)[0].toLocaleString(...(args.slice(0, -1))),
+            "$dir": (...args) => console.dir(...args),
+            "qwerty": "asdfgh",
         },
-        render: (tokens, idx) => {
-            const m = tokens[idx].info.trim().match(/^spoiler\s+(.*)$/);
-            if (tokens[idx].nesting === 1) {
-                // opening tag
-                return '<details><summary>' + escapeHtml(m[1]) + '</summary>\n';
-            } else {
-                // closing tag
-                return '</details>\n';
-            }
+        components: {
+            Greeting: (props) => `Hello, ${props.to}! ${props.children}`,
         },
-    }],
-});
-document.write(buf.toString());
-document.close();
+        markdownCustomContainers: [{
+            name: 'content',
+        }, {
+            name: 'spoiler',
+            validate: (params) => {
+                return params.trim().match(/^spoiler\s+(.*)$/);
+            },
+            render: (tokens, idx) => {
+                const m = tokens[idx].info.trim().match(/^spoiler\s+(.*)$/);
+                if (tokens[idx].nesting === 1) {
+                    // opening tag
+                    return '<details><summary>' + escapeHtml(m[1]) + '</summary>\n';
+                } else {
+                    // closing tag
+                    return '</details>\n';
+                }
+            },
+        }],
+    }, cf || {}));
+    document.write(buf.toString());
+    document.close();
 });
---
+++ src.dist/mkd+lsx.js
@@ -0,0 +1,11 @@
+const start = (async (cf) => {
+    const buf = await menneu.render(Array.from(document.querySelectorAll('script[type="text/markdown"]'), x => x.innerHTML).join(' '), {}, Object.assign({
+        rawInput: false,
+        inputFormat: 'md',
+        outputFormat: 'html',
+        title: 'Markdown',
+        markdownBodyStyle: 'font-family: "Yu Gothic Medium", YuGothic, meiryo, "Microsoft JhengHei", "Microsoft YaHei", "SimHei", helvetica, arial, sans-serif;',
+    }, cf || {}));
+    document.write(buf.toString());
+    document.close();
+});
