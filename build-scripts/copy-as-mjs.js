88fa10aeba9ee6d161298daf50bcd29183e12e47
shellyln <shellyln@users.noreply.github.com>
2019-03-25 22:41:04 +0900

    Add build script copying '.mjs' files.

---
+++ build-scripts/copy-as-mjs.js
@@ -0,0 +1,30 @@
+
+const fs = require('fs');
+const path = require('path');
+
+
+
+function copyAsMjs(srcDir, destDir, options) {
+    const opts = Object.assign({}, {
+        srcExt: '.js',
+        destExt: '.mjs',
+    }, options || {});
+
+    fs.mkdirSync(destDir, { recursive: true });
+
+    if (fs.lstatSync(srcDir).isDirectory()) {
+        const files = fs.readdirSync(srcDir);
+        for (const entry of files) {
+            const srcEntryPath = path.join(srcDir, entry);
+            if (fs.lstatSync(srcEntryPath).isDirectory()) {
+                copyAsMjs(srcEntryPath, path.join(destDir, entry));
+            } else {
+                if (entry.toLowerCase().endsWith(opts.srcExt)) {
+                    fs.copyFileSync(srcEntryPath, path.join(destDir, entry.slice(0, -(opts.srcExt.length)) + opts.destExt));
+                }
+            }
+        }
+    }
+}
+
+exports.copyAsMjs = copyAsMjs;
